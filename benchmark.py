2cdb05704e9cd10117e2d2b4dcad26ae11bf1742
rlfogh <rlfogh@61c343ba-a409-11dd-bd46-993b494de23d>
2008-11-05 13:18:53 +0000

    changed code_object representation, variables impl. begun

--- benchmark.py
+++ benchmark.py
@@ -2,11 +2,14 @@
 
 #t1 = time.time()
 
-class point:
+class Point:
   x = 2
   y = 3
   def getPoint():
     return 1
+
+myPoint = Point()
+print myPoint.getPoint()
 
 #t2 = time.time()
 #print "Printing 'hello world!' took %d seconds" % (t2-t1)
