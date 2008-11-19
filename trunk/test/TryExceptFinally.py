c5639e9fa74184b04cdd54b9793c0d8becdef20f
ubbewelling <ubbewelling@61c343ba-a409-11dd-bd46-993b494de23d>
2008-11-19 08:29:31 +0000

    fixed python 2.4 errors

--- trunk/test/TryExceptFinally.py
+++ trunk/test/TryExceptFinally.py
@@ -1,16 +1,23 @@
 i = 0
+
+
 try:
-  i = i + 10
-  raise
-  i = i + 100
-except:
-  i = i + 10
+  try:
+    i = i + 10
+    raise
+    i = i + 100
+  except:
+    i = i + 10
 finally:
   i = i + 10
+
+
 try:
-  i = i + 10
-except:
-  i = i + 100
+  try:
+    i = i + 10
+  except:
+    i = i + 100
 finally:
   i = i + 2
 print i
+
