0797da7b5952ff020fb5c59a964dad307bbde9a3
rlfogh <rlfogh@61c343ba-a409-11dd-bd46-993b494de23d>
2008-11-19 08:22:04 +0000

    Added tests and implemented try-except-finally, raise and a block stack to support raise, break and continue.

--- src/pejs_library.js
+++ src/pejs_library.js
@@ -4,10 +4,10 @@
 // [Opcode, Offset, (Arg, Arg Type, Arg Value,)? Opcode Name]
 
 var pejs_library = new Object();
-    pejs_library.co_name = "?";
+    pejs_library.co_name = "<module>";
     pejs_library.co_argcount = 0;
     pejs_library.co_nlocals = 0;
-    pejs_library.co_varnames = ["False", "range", "True", "str"];
+    pejs_library.co_varnames = [];
     pejs_library.co_cellvars = [];
     pejs_library.co_freevars = [];
     pejs_library.co_code =     [ //Instructions
@@ -15,10 +15,10 @@ var pejs_library = new Object();
       [90,3,0,"GLOBAL VARIABLE","True","STORE_NAME"],
       [100,6,1,"CONSTANT","0","LOAD_CONST"],
       [90,9,1,"GLOBAL VARIABLE","False","STORE_NAME"],
-      [100,12,2,"CONSTANT","<code object str at 0xb7ec37a0, file \"pejs_library.py\", line 4>","LOAD_CONST"],
+      [100,12,2,"CONSTANT","<code object str at 0xb7d5ee78, file \"pejs_library.py\", line 4>","LOAD_CONST"],
       [132,15,0,"OTHER","0","MAKE_FUNCTION"],
       [90,18,2,"GLOBAL VARIABLE","str","STORE_NAME"],
-      [100,21,3,"CONSTANT","<code object range at 0xb7ec3b20, file \"pejs_library.py\", line 7>","LOAD_CONST"],
+      [100,21,3,"CONSTANT","<code object range at 0xb7d6c7b8, file \"pejs_library.py\", line 7>","LOAD_CONST"],
       [132,24,0,"OTHER","0","MAKE_FUNCTION"],
       [90,27,3,"GLOBAL VARIABLE","range","STORE_NAME"],
       [100,30,4,"CONSTANT","None","LOAD_CONST"],
@@ -46,7 +46,7 @@ var pejs_library_str = new Object();
       [83,7,"RETURN_VALUE"]
     ];
     pejs_library_str.co_consts = ["None", ""];
-    pejs_library_str.co_names = ["value"];
+    pejs_library_str.co_names = [];
     pejs_library_str.co_filename = pejs_library.py;
     pejs_library_str.co_firstlineno = 4;
     pejs_library_str.co_stacksize = 2;
@@ -67,7 +67,7 @@ var pejs_library_range = new Object();
       [83,9,"RETURN_VALUE"]
     ];
     pejs_library_range.co_consts = ["None"];
-    pejs_library_range.co_names = ["start", "end"];
+    pejs_library_range.co_names = [];
     pejs_library_range.co_filename = pejs_library.py;
     pejs_library_range.co_firstlineno = 7;
     pejs_library_range.co_stacksize = 2;
---
+++ test/ObjectHandling.py
@@ -0,0 +1,6 @@
+class MyObject():
+  myField = 0
+  def __init__(self):
+    self.myField = 42
+
+print MyObject().myField
